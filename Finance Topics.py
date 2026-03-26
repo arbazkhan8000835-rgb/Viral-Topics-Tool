dd63cbbbce43400c91cfa0cf903638cd8494ed39
arbazkhan8000835-rgb <arbazkhan8000835@gmail.com>
2026-03-27 02:23:03 +0500

    Update Finance Topics.py

--- Finance Topics.py
+++ Finance Topics.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 
 # YouTube API Key
 
-API_KEY = "Enter your API Key here"
+API_KEY = "AIzaSyDKBxqXFE8ktMBkjaYUORER3T3wL0ODPaU"
 
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 
